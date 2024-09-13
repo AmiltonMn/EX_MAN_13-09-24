5e383aecdca65c4f13860225215cb3f3496dd311
AmiltonMn <amilton.moreiran@hotmail.com>
2024-09-13 16:23:59 -0300

    Exercício 10

---
+++ somaPorcentagemPeso.py
@@ -0,0 +1,8 @@
+# https://github.com/Andredev-dias/Base-de-Algoritmos-em-C/blob/master/aulapratica25.c
+
+peso = float(input('Digite o seu peso:\n'))
+
+pesoMais15 = peso + (peso * 0.15)
+pesoMenos20 = peso - (peso * 0.20)
+
+print(f'Seu peso mais 15% é {pesoMais15}\nSeu peso menos 20% é {pesoMenos20}')
