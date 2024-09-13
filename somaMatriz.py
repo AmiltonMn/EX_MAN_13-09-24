6c33851a72ac9637d0a7c75a853a3949fca3e0f6
AmiltonMn <amilton.moreiran@hotmail.com>
2024-09-13 15:24:59 -0300

    Exercício 6

---
+++ somaMatriz.py
@@ -0,0 +1,8 @@
+# https://github.com/Andredev-dias/Base-de-algoritmos-em-C-v3/blob/master/exer12funcoes.c
+
+def quadrado(valor):
+    return valor**2
+    
+valor = int(input('Digite um valor para calcular o quadrado dele:\n'))
+
+print(quadrado(valor))
