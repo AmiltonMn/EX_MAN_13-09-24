7231032270883ac014fbf9f7eab89b9e0a51b3fe
AmiltonMn <amilton.moreiran@hotmail.com>
2024-09-13 14:42:13 -0300

    Exercício 2

---
+++ horasTrabalhadas.py
@@ -0,0 +1,15 @@
+# https://github.com/Andredev-dias/Base-de-Algoritmos-em-C/blob/master/aulapratica26.c
+
+try:
+    dias = int(input('Digite o número de dias!'))
+except:
+    print('Valor digitado inválido!')
+    
+hora = 0
+resp = 0
+    
+for i in range(dias):
+    hora = int(input(f'Digite a hora do {i}° dia: \n'))
+    resp += hora
+    
+print(f'A média de horas trabalhadas é de: {resp/dias}h por dia!')
